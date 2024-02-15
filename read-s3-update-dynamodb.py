07f4b19c32c663d372faf9ac54824eb8743bac23
Mark Kibara <markkibara2014@gmail.com>
2024-02-15 21:33:58 +0300

    Add first lambda function

--- read-s3-update-dynamodb.py
+++ read-s3-update-dynamodb.py
@@ -1,44 +1,56 @@
-import boto3
 import json
+import boto3
+import csv
 import os
 
 def lambda_handler(event, context):
-    # Define DynamoDB and SNS clients
-    dynamo_table_name = 'InventoryTable'
+   
+    # Get the S3 bucket and object information from the event
+    s3_bucket = event['Records'][0]['s3']['bucket']['name']
+    s3_key = event['Records'][0]['s3']['object']['key']
+
+    # Test lambda function manually
+    # s3_bucket = 'productinventory240215'
+    # s3_key = 'inventory.csv'
+
+    # Set the DynamoDB table name
+    dynamodb_table_name = 'InventoryTable'
+
+    # Create a DynamoDB client
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table(dynamo_table_name)
-    sns = boto3.client('sns')
-    region = os.environ['AWS_REGION']
-    account_id = context.invoked_function_arn.split(":")[4]
+    table = dynamodb.Table(dynamodb_table_name)
 
-    # Scan DynamoDB table to get items with count of 5 or less than 5
-    response = table.scan(
-        FilterExpression=boto3.dynamodb.conditions.Attr('ProductCount').lt(6)
-    )
-    
-    # Check if there are items with count less than 5
-    items = response.get('Items', [])
-    
-    if items:
-        # Prepare notification message
-        notification_message = "Low inventory alert:\n"
-        for item in items:
-            notification_message += f"StoreRegion: {item['StoreRegion']}, Item: {item['Product']}, Count: {item['ProductCount']}\n"
+    # Create an S3 client
+    s3 = boto3.client('s3')
 
-        # Publish notification to SNS
-        sns.publish(
-            # TopicArn='arn:aws:sns:us-east-1:471285348599:LowInventoryTopic',
-            TopicArn = f'arn:aws:sns:{region}:{account_id}:LowInventoryTopic',
-            Message=notification_message,
-            Subject='Low Inventory Alert'
-        )
+    # Download the CSV file from S3
+    temp_file_path = '/tmp/temp.csv'
+    s3.download_file(s3_bucket, s3_key, temp_file_path)
+
+    # Read and process the CSV file, skipping the header row
+    with open(temp_file_path, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        # Skip the header row
+        next(csvreader, None)
         
-        return {
-            'statusCode': 200,
-            'body': 'Notification sent successfully.'
-        }
-    else:
-        return {
-            'statusCode': 200,
-            'body': 'No items with count less than 5.'
-        }
+        for row in csvreader:
+            store = row[0]
+            product = row[1]
+            count = int(row[2])
+
+            # Update DynamoDB table
+            table.put_item(
+                Item={
+                    'StoreRegion': store,
+                    'Product': product,
+                    'ProductCount': count
+                }
+            )
+  
+    # Clean up the temporary file
+    os.remove(temp_file_path)
+
+    return {
+        'statusCode': 200,
+        'body': 'CSV processing complete'
+    }
